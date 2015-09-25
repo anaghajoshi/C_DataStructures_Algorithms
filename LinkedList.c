0db69773a2fe12bedcf2fa3f0c3244a44cac6d65
Anagha Joshi <anagha.elex@gmail.com>
2015-09-24 20:02:02 -0400

    Add First Program Template

---
+++ LinkedList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Linked List Implementation in C */
+
+struct node{
+	int data;
+	struct node *next;	
+};
+
+int main(){
+
+node * A;
+A = NULL: //The list is empty
+
+return 0;
+}
+
